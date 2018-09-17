b227f9f47e640450d79e163fce95685ffae886ba
zenott <mblevel@gmail.com>
2018-09-17 17:36:08 +0200

    Add axes

--- index.js
+++ index.js
@@ -20,15 +20,16 @@ document.addEventListener('DOMContentLoaded', function() {
                quarter = 'Q4';
                break;
          }
-         gdp.push([datesplit[0] + ' ' + quarter, Number(element[1])]);
+         gdp.push([new Date(element[0]), Number(element[1]), datesplit[0] + ' ' + quarter]);
       });
+      console.log(gdp);
 
       d3.select('#container')
          .append('h1')
          .text('United States GDP');
 
-      const w = 1400;
-      const h = 600;
+      const w = 900;
+      const h = 500;
 
       const svg = d3
          .select('body')
@@ -36,25 +37,44 @@ document.addEventListener('DOMContentLoaded', function() {
          .attr('width', w)
          .attr('height', h);
 
+      const padding = 40;
+      const xScale = d3
+         .scaleTime()
+         .domain([
+            d3.min(gdp, (d, i) => d[0]),
+            d3.max(gdp, (d, i) => d[0])
+         ])
+         .range([padding, w - padding]);
+      const yScale = d3
+         .scaleLinear()
+         .domain([0, d3.max(gdp, d => d[1])])
+         .range([h - padding, padding]);
+
       svg.selectAll('rect')
          .data(gdp)
          .enter()
          .append('rect')
-         .attr('x', (d, i) => {
-            return i * 4;
-         })
-         .attr('y', d => {
-            return h - d[1] / 30;
-         })
+         .attr('x', (d, i) => xScale(d[0]))
+         .attr('y', d => yScale(d[1]))
          .attr('width', 3)
-         .attr('height', d => {
-            return d[1] / 30;
-         })
+         .attr('height', d => h-padding-yScale(d[1]))
          .attr('fill', 'royalblue')
          .attr('class', 'bar')
          .append('title')
          .text(d => {
-            return d[0] + '\n$' + d[1] + ' Billion';
+            return d[2] + '\n$' + d[1] + ' Billion';
          });
+
+      const xAxis = d3.axisBottom(xScale);
+
+      svg.append("g")
+         .attr("transform", "translate(0, " + (h - padding) + ")")
+         .call(xAxis);
+
+      const yAxis = d3.axisLeft(yScale);
+
+      svg.append('g')
+         .attr('transform', 'translate('+ padding + ',0)')
+         .call(yAxis);
    });
 });
